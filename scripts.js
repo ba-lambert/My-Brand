f07fb22ffcd461f35954ff233f3cb81cff571a87
Ba-lambert <50544115+mugwanezalambert@users.noreply.github.com>
2023-01-13 16:54:10 +0200

    final ui

--- scripts.js
+++ scripts.js
@@ -8,3 +8,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",(
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+const blogcontainer = [...document.querySelectorAll('.blog-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+blogcontainer.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
